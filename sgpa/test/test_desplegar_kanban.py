8b5ebe416a14f12d20c5fb942a84a0170851756b
Gustavo_Otazu <gustavootazu2010@hotmail.com>
2016-05-19 11:09:05 -0400

    test para desplegar kanban

---
+++ sgpa/test/test_desplegar_kanban.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from sgpa.models import Proyecto
+from django.test import Client
+
+
+class SGPATestCase(TestCase):
+	def test_desplegar_kanban(self):
+	
+	        c = Client()
+	        c.login(username='gustavo', password='gustavo')
+		print('\n------Ejecutando test para desplegar kanban-------\n')
+
+	        resp = c.get('/usuario/desplegar_kanban/1')
+	        self.assertTrue(resp.status_code, 300)
